6671e4d623ea03c0401fb595aa6051b02724e11d
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-02-26 18:05:11 -0500

    all questions prompting correctly

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,184 @@
-// TODO: Include packages needed for this application
+/*-------------------------------------------------------------------- 
+-                              PACKAGES 
+--------------------------------------------------------------------*/ 
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+/*-------------------------------------------------------------------- 
+-                              ARRAYS
+--------------------------------------------------------------------*/ 
 
-// TODO: Create a function to initialize app
-function init() {}
+const questions = [
+  'Project Name: ',
+  'Project Description: ',
+  'Installation Instructions: ',
+  'Usage Information: ',
+  'Contribution Guidelines: ',
+  'Test Instructions: ',
+];
 
-// Function call to initialize app
+const answers = [
+
+];
+
+/*-------------------------------------------------------------------- 
+-                             WRITE FILES
+--------------------------------------------------------------------*/ 
+
+function writeToFile(fileName, data) {
+
+};
+
+/*-------------------------------------------------------------------- 
+-                             APPLICATION
+--------------------------------------------------------------------*/ 
+
+const init = () => {
+  return inquirer.prompt([
+
+    /* --- QUESTION 1: PROJECT NAME ---*/
+    {
+      type: 'input',
+      name: 'projName',
+      message: 'Project Name: ',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } 
+        else {
+          console.log('Project name is mandatory!');
+          return false;
+        }
+      }
+    },
+
+    /* --- QUESTION 2: PROJECT DESCRIPTION ---*/
+    {
+      type: 'input',
+      name: 'projDescription',
+      message: 'Project Description: ',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } 
+        else {
+          console.log('Provide a brief description of your project: ');
+          return false;
+        }
+      }
+    },
+
+    /* --- QUESTION 3: INSTALL INSTRUCTIONS ---*/
+    {
+      type: 'input',
+      name: 'projInstallInst',
+      message: 'Installation Instructions: ',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } 
+        else {
+          console.log('Project name is mandatory!');
+          return false;
+        }
+      }
+    },
+
+    /* --- QUESTION 4: USAGE INFO ---*/
+    {
+      type: 'input',
+      name: 'projUsageInfo',
+      message: 'Usage Information: ',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } 
+        else {
+          console.log('Usage information is required!');
+          return false;
+        }
+      }
+    },
+
+    /* --- QUESTION 5: CONTRIBUTION GUIDELINES  ---*/
+    {
+      type: 'input',
+      name: 'projContGuide',
+      message: 'Contribution Guidelines: ',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } 
+        else {
+          console.log('Contribution guidelines are required!');
+          return false;
+        }
+      }
+    },
+    
+    /* --- QUESTION 6: TEST INSTRUCTIONS  ---*/
+    {
+      type: 'input',
+      name: 'projTest',
+      message: 'Test Instructions: ',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } 
+        else {
+          console.log('Test instructions are required!');
+          return false;
+        }
+      }
+    },
+    
+    /* --- QUESTION 7: LICENSES ---*/
+    {
+      type: 'checkbox',
+      name: 'languages',
+      message: 'What did you build this project with? (Check all that apply)',
+      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+    },
+
+    /* --- QUESTION 8: GitHub USERNAME  ---*/
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your Github Username (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } 
+        else {
+          console.log('Please enter your github username!');
+          return false;
+        }
+      }
+    },
+
+     /* --- QUESTION 9: EMAIL  ---*/
+     {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email: (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } 
+        else {
+          console.log('Please enter your email!');
+          return false;
+        }
+      }
+    }
+  ])
+  
+};
+
+/*-------------------------------------------------------------------- 
+-                          CALL APPLICATION
+--------------------------------------------------------------------*/ 
+
 init();
+
